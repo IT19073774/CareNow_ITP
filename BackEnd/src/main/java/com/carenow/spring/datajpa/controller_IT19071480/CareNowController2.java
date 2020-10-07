1398e7e942adc6de20a4dd53de53abe0b698fae9
IT19073774 <IT19073774@my.sliit.lk>
2020-10-07 19:28:44 +0530

    Fix Errors Commit

--- BackEnd/src/main/java/com/carenow/spring/datajpa/controller_IT19071480/CareNowController2.java
+++ BackEnd/src/main/java/com/carenow/spring/datajpa/controller_IT19071480/CareNowController2.java
@@ -33,8 +33,8 @@ public class CareNowController2 {
 	}	
 
 	@GetMapping(path ="/drugs")
-	<List<Drug>> getDrugs() {
-		return drugServices.findAllDrugs();
+	List<Drug> getDrugs() {
+		return drugServices.findAll();
 	}
 
 ////////////////////////////////////////////////////////////////////////////////
